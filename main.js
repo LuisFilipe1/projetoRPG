031ff61cd212bc6a7f543bf04a9a9d10ce9a45ea
LuisFilipe1 <filipeluisp25@gmail.com>
2024-03-02 12:19:51 +0000

    ajuste nas pastas e criação da main

---
+++ main.js
@@ -0,0 +1,11 @@
+let raceSelect = document.getElementById("raceSelect")
+if (raceSelect = Anao){
+    
+}
+let classSelect = document.getElementById("classSelect")
+if (classSelect = Barbaro){
+
+}
+if (classSelect = Mago){
+    
+}
