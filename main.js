a073fac41ee80074e406514376a4cafb179b2d4f
LuisFilipe1 <filipeluisp25@gmail.com>
2024-03-06 20:30:04 -0300

    inclusão dos atributos, código js e estilização com CSS

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ if (classSelect = Barbaro){
 if (classSelect = Mago){
     
 }
+/*let force = Number(rl.question(`Quanto é sua força ? `));
+let modifforce = Math.floor(parseFloat(force - 10) / 2);
+console.log(`modif: ${modifforce}`);
+let dext = Number(rl.question(`Quanto é sua destreza ? `));
+let modifdext = Math.floor(parseFloat(dext - 10) / 2);
+console.log(`modif: ${modifdext}`);
+let consti = Number(rl.question(`Quanto é sua constituição ? `));
+let modiffconsti = Math.floor(parseFloat(consti - 10) / 2);
+console.log(`modif: ${modiffconsti}`);
+let intel = Number(rl.question(`Quanto é sua intligência ? `));
+let modifintel = Math.floor(parseFloat(intel - 10) / 2);
+console.log(`modif: ${modifintel}`);
+let char = Number(rl.question(`Quanto é sua carisma ? `));
+let modifchar = Math.floor(parseFloat(char - 10) / 2);
+console.log(`modif: ${modifchar}`);
+let wis = Number(rl.question(`Quanto é sua sabedoria ? `));
+let modifwis = Math.floor(parseFloat(wis - 10) / 2);
+console.log(`modif: ${modifwis}`);
+*/
