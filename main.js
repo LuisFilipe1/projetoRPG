4e66d945cb4396f5138d044df9acaad69d38033c
LuisFilipe1 <filipeluisp25@gmail.com>
2024-04-19 20:15:42 +0000

    Mudança de background, criação da lógica no JS do calculo dos atributos e retorno no HTML

--- main.js
+++ main.js
@@ -1,40 +1,30 @@
-function trocaTema(body)
-{
-    body.style.backgroundColor="blue"
-}
-
-
 let raceSelect = document.getElementById("raceSelect");
+//calculo dos pontos de vida
+const calculoPontosVida = raceSelect
 //Aqui lista dos atributos e o calculo do modificador.
- 
-const force = document.geatElementById("force");
-atributoForce = Math.floor(parseFloat(force - 10)/2);
-const dext = document.getElementById("desstreza");
-atributoDestreza = Math.floor(parseFloat(dext - 10)/2);
-const consti = document.getElementById("constituicao");
-atributoConsti = Math.floor(parseFloat(consti - 10)/2);
-const intel = document.getElementById("inteligencia");
-atributoIntel = Math.floor(parseFloat(intel- 10)/2);
-const wis = document.getElementById("sabedoria");
-atributoWis = Math.floor(parseFloat(wis - 10)/2);
-const char = document.getElementById("carisma");
-atributoChar = Math.floor(parseFloat(char - 10)/2);
+/* function calculoModif() {
+    const force = document.getElementById("force");
+    atributoForce = Math.floor(parseFloat(force - 10) / 2);
+    document.getElementById("modifForce").ariaValueNow = atributoForce
+    const dext = document.getElementById("desstreza");
+    atributoDestreza = Math.floor(parseFloat(dext - 10) / 2);
+    const consti = document.getElementById("constituicao");
+    atributoConsti = Math.floor(parseFloat(consti - 10) / 2);
+    const intel = document.getElementById("inteligencia");
+    atributoIntel = Math.floor(parseFloat(intel - 10) / 2);
+    const wis = document.getElementById("sabedoria");
+    atributoWis = Math.floor(parseFloat(wis - 10) / 2);
+    const char = document.getElementById("carisma");
+    atributoChar = Math.floor(parseFloat(char - 10) / 2);
+} */
 console.log = calculoAtributos
-if (raceSelect = Anao){
-    
+if (raceSelect = Anao) {
+
 }
 let classSelect = document.getElementById("classSelect")
-if (classSelect = Barbaro){
+if (classSelect = Barbaro) {
 
 }
-if (classSelect = Mago){
-    
+if (classSelect = Mago) {
+
 }
-/*
-let char = Number(rl.question(`Quanto é sua carisma ? `));
-let modifchar = Math.floor(parseFloat(char - 10) / 2);
-console.log(`modif: ${modifchar}`);
-let wis = Number(rl.question(`Quanto é sua sabedoria ? `));
-let modifwis = Math.floor(parseFloat(wis - 10) / 2);
-console.log(`modif: ${modifwis}`);
-*/
